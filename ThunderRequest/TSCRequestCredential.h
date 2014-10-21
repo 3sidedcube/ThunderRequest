2f37cfe4ec86c74d594d65c0e37d7ed23f9d4ce9
Matt Cheetham <matt@3sidedcube.com>
2014-10-21 13:54:12 +0100

    Adds documentation to TSCRequestCredential

--- ThunderRequest/TSCRequestCredential.h
+++ ThunderRequest/TSCRequestCredential.h
@@ -10,13 +10,42 @@
 
 @interface TSCRequestCredential : NSObject
 
+/**
+ @abstract The username to be sent by `TSCRequestController` for authentication requests
+ */
 @property (nonatomic, strong) NSString *username;
+
+/**
+ @abstract The password to be sent by `TSCRequestController` for authentication requests
+ */
 @property (nonatomic, strong) NSString *password;
+
+/**
+ @abstract The authorisation token to be sent by `TSCRequestController` for authentication requests
+ */
 @property (nonatomic, strong) NSString *authorizationToken;
 
+/**
+ @abstract The credential created using the `username`, `password` and/or `authorizationToken` credentials
+ @discussion The credential will be created after initialising this object using `initWithUsername:password` or `initWithAuthorizationToken`
+ */
 @property (nonatomic, strong) NSURLCredential *credential;
 
+///---------------------------------------------------------------------------------------
+/// @name Initialization
+///---------------------------------------------------------------------------------------
+
+/**
+ Initializes the credentials object.
+ @param username The username to be sent by `TSCRequestController` for authentication requests.
+ @param password The password to be sent by `TSCRequestController` for authentication requests
+ */
 - (id)initWithUsername:(NSString *)username password:(NSString *)password;
+
+/**
+ Initializes the credentials object.
+ @param authorizationToken The authorizationToken to be sent by `TSCRequestController` for authentication requests.
+ */
 - (id)initWithAuthorizationToken:(NSString *)authorizationToken;
 
 @end
