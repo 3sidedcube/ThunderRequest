85e8709c8f128abe4f74babf625784af92ec74e3
Matt Cheetham <matt@3sidedcube.com>
2014-10-21 12:06:24 +0100

    Adds documentation for TSCRequestController

--- ThunderRequest/TSCRequestController.h
+++ ThunderRequest/TSCRequestController.h
@@ -9,15 +9,52 @@
 #import <Foundation/Foundation.h>
 #import "TSCRequest.h"
 
+/**
+ A `TSCRequestController` object lets you asynchronously load the contents of a URL with a block returned upon completion.
+ 
+ The `TSCRequestController` object should be added as a property and retained for use with multiple requests.
+ Generally one `TSCRequestController` should be initialised and shared per API/Base URL.
+ 
+ To use a `TSCRequestController` do the following:
+ 
+ 1. Create a property with the type `TSCRequestController`
+ 2. Initialise the controller with either the `initWithBaseURL:` or `initWithBaseAddress:` method.
+ 3. Use any of the GET, POST, PUT, DELETE or HEAD methods to perform an asynchronous web request
+ 
+ */
 @interface TSCRequestController : NSObject
 
+/**
+ @abstract The shared Base URL for all requests routed through the controller
+ @discussion This is most commonly set using the `initWithBaseURL:` or `initWithBaseAddress:` methods.
+ */
 @property (nonatomic, strong) NSURL *sharedBaseURL;
+
+/**
+ @abstract The shared request headers for all requests routed through the controller
+ */
 @property (nonatomic, strong) NSMutableDictionary *sharedRequestHeaders;
+
+/**
+ @abstract The shared content type for all requests routed through the controller
+ */
 @property (nonatomic, assign) TSCRequestContentType sharedContentType;
+
+/**
+ @abstract The shared request credentials for all requests routed through the controller
+ */
 @property (nonatomic, strong) TSCRequestCredential *sharedRequestCredential;
+
+///---------------------------------------------------------------------------------------
+/// @name Initialization
+///---------------------------------------------------------------------------------------
 
 - (id)initWithBaseURL:(NSURL *)baseURL;
 - (id)initWithBaseAddress:(NSString *)baseAddress;
+
+///---------------------------------------------------------------------------------------
+/// @name GET requests
+///---------------------------------------------------------------------------------------
 
 /**
  Performs a GET request on the base URL.
@@ -34,13 +71,9 @@
  */
 - (void)get:(NSString *)path withURLParamDictionary:(NSDictionary *)URLParamDictionary completion:(TSCRequestCompletionHandler)completion;
 
-/**
- Performs a GET request on the base URL using the supplied object to build the URL.
- @param path The path to be appended to the base URL.
- @param URLParamObject Object used to build the URL.
- @param completion The completion block that will be fired once the request has completed.
- */
-//- (void)get:(NSString *)path withURLParamObject:(NSObject *)URLParamObject completion:(PCRequestCompletionHandler)completion;
+///---------------------------------------------------------------------------------------
+/// @name POST requests
+///---------------------------------------------------------------------------------------
 
 /**
  Performs a POST request on the base URL using the supplied bodyParams dictionary as the POST body.
@@ -57,12 +90,21 @@
  @param bodyParams The dictionary used in the POST body.
  @param completion The completion block that will be fired once the request has completed.
  */
-
 - (void)post:(NSString *)path withURLParamDictionary:(NSDictionary *)URLParamDictionary bodyParams:(NSDictionary *)bodyParams completion:(TSCRequestCompletionHandler)completion;
 
+/**
+ Performs a POST request on the base URL using the supplied paramater dictionary to build the URL, and bodyParams dictionary as the POST body.
+ @param path The path to be appended to the base URL.
+ @param URLParamDictionary Dictionary used to build the URL.
+ @param bodyParams The dictionary used in the POST body.
+ @param contentType The type of `TSCRequestContentType` to be used when encoding the request body
+ @param completion The completion block that will be fired once the request has completed.
+ */
 - (void)post:(NSString *)path withURLParamDictionary:(NSDictionary *)URLParamDictionary bodyParams:(NSDictionary *)bodyParams contentType:(TSCRequestContentType)contentType completion:(TSCRequestCompletionHandler)completion;
 
-//- (void)post:(NSString *)path withURLParamObject:(NSDictionary *)URLParamObject bodyParams:(NSDictionary *)bodyParams completion:(PCRequestCompletionHandler)completion;
+///---------------------------------------------------------------------------------------
+/// @name PUT requests
+///---------------------------------------------------------------------------------------
 
 /**
  Performs a PUT request on the base URL using the supplied bodyParams dictionary as the PUT body.
@@ -70,7 +112,6 @@
  @param bodyParams The dictionary used in the PUT body.
  @param completion The completion block that will be fired once the request has completed.
  */
-
 - (void)put:(NSString *)path bodyParams:(NSDictionary *)bodyParams completion:(TSCRequestCompletionHandler)completion;
 
 /**
@@ -80,11 +121,17 @@
  @param bodyParams The dictionary used in the PUT body.
  @param completion The completion block that will be fired once the request has completed.
  */
-
 - (void)put:(NSString *)path withURLParamDictionary:(NSDictionary *)URLParamDictionary bodyParams:(NSDictionary *)bodyParams completion:(TSCRequestCompletionHandler)completion;
 
-//- (void)put:(NSString *)path withURLParamObject:(NSDictionary *)URLParamObject bodyParams:(NSDictionary *)bodyParams completion:(PCRequestCompletionHandler)completion;
+///---------------------------------------------------------------------------------------
+/// @name DELETE requests
+///---------------------------------------------------------------------------------------
 
+/**
+ Performs a DELETE request on the base URL using the supplied paramater dictionary to build the URL.
+ @param path The path to be appended to the base URL.
+ @param completion The completion block that will be fired once the request has completed.
+ */
 - (void)delete:(NSString *)path completion:(TSCRequestCompletionHandler)completion;
 
 /**
@@ -94,6 +141,10 @@
  @param completion The completion block that will be fired once the request has completed.
  */
 - (void)delete:(NSString *)path withURLParamDictionary:(NSDictionary *)URLParamDictionary completion:(TSCRequestCompletionHandler)completion;
+
+///---------------------------------------------------------------------------------------
+/// @name HEAD requests
+///---------------------------------------------------------------------------------------
 
 /**
  Performs a HEAD request on the base URL using the supplied paramater dictionary to build the URL.
