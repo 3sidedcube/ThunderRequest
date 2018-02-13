0c2560e7179dbccaa351f38b4f9c0182a1a8ce93
Matthew Cheetham <matt@3sidedcube.com>
2018-02-13 11:55:37 +0000

    Fixes test targets

--- ThunderRequestTests/ThunderRequest-KeychainTests.swift
+++ ThunderRequestTests/ThunderRequest-KeychainTests.swift
@@ -7,7 +7,7 @@
 //
 
 import XCTest
-import ThunderRequest
+@testable import ThunderRequest
 
 class ThunderRequest_KeychainTests: XCTestCase {
     
@@ -25,91 +25,24 @@ class ThunderRequest_KeychainTests: XCTestCase {
         
         let credential = TSCRequestCredential(username: "test", password: "123")
         
-        XCTAssertNotNil(credential.username, "Username is nil")
-        XCTAssertNotNil(credential.password, "Password is nil")
-        XCTAssertNotNil(credential.credential, "Credential is nil")
+        XCTAssertNotNil(credential?.username, "Username is nil")
+        XCTAssertNotNil(credential?.password, "Password is nil")
+        XCTAssertNotNil(credential?.credential, "Credential is nil")
     }
     
     func testInitialiseAuthTokenCredential() {
         
         let credential = TSCRequestCredential(authorizationToken: "SHADSJMAS")
         
-        XCTAssertNotNil(credential.authorizationToken, "Authorization Token is nil")
+        XCTAssertNotNil(credential?.authorizationToken, "Authorization Token is nil")
     }
     
     func testInitialiseOAuth2Credential() {
         
-        let credential = TSCOAuth2Credential(authorizationToken: "saDHSAHF", refreshToken: "DSAHJDSA", expiryDate: NSDate(timeIntervalSinceNow: 24))
+        let credential = TSCOAuth2Credential(authorizationToken: "saDHSAHF", refreshToken: "DSAHJDSA", expiryDate: Date(timeIntervalSinceNow: 24))
         
-        XCTAssertNotNil(credential.authorizationToken, "Authorization Token is nil")
-        XCTAssertNotNil(credential.refreshToken, "Refresh Token is nil")
-        XCTAssertNotNil(credential.expirationDate, "Expiry Date is nil")
-    }
-    
-    func testPasswordUsernameCredentialToKeychain() {
-        
-        let credential = TSCRequestCredential(username: "test", password: "123")
-        
-        TSCRequestCredential.storeCredential(credential, withIdentifier: "credential")
-        
-        let pulledCredential = TSCRequestCredential.retrieveCredentialWithIdentifier("credential")
-        
-        XCTAssertNotNil(pulledCredential, "Credential was not saved or pulled from keychain")
-        XCTAssertNotNil(pulledCredential.username, "Credential username was retrieved as nil")
-        XCTAssertNotNil(pulledCredential.password, "Credential password was retrieved as nil")
-        XCTAssertNotNil(pulledCredential.credential, "Credential credential was retrieved as nil")
-        
-        TSCRequestCredential.deleteCredentialWithIdentifier("credential")
-        
-        let deletedCredential = TSCRequestCredential.retrieveCredentialWithIdentifier("credential")
-        
-        XCTAssertNil(deletedCredential, "Credential was not deleted correctly from the keychain")
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        
-    }
-    
-    func testAuthTokenCredentialToKeychain() {
-        
-        let credential = TSCRequestCredential(authorizationToken: "TOKEN")
-        
-        TSCRequestCredential.storeCredential(credential, withIdentifier: "credential")
-        
-        let pulledCredential = TSCRequestCredential.retrieveCredentialWithIdentifier("credential")
-        
-        XCTAssertNotNil(pulledCredential, "Credential was not saved or pulled from keychain")
-        XCTAssertNotNil(pulledCredential.authorizationToken, "Credential token was retrieved as nil")
-        
-        TSCRequestCredential.deleteCredentialWithIdentifier("credential")
-        
-        let deletedCredential = TSCRequestCredential.retrieveCredentialWithIdentifier("credential")
-        
-        XCTAssertNil(deletedCredential, "Credential was not deleted correctly from the keychain")
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        
-    }
-    
-    func testOAuth2CredentialToKeychain() {
-        
-        let credential = TSCOAuth2Credential(authorizationToken: "TOKEN", refreshToken: "REFRESHTOKEN", expiryDate: NSDate(timeIntervalSinceNow: 24))
-        
-        TSCOAuth2Credential.storeCredential(credential, withIdentifier: "credential")
-        
-        let pulledCredential = TSCOAuth2Credential.retrieveCredentialWithIdentifier("credential")
-        
-        XCTAssertNotNil(pulledCredential, "Credential was not saved or pulled from keychain")
-        XCTAssertNotNil(pulledCredential.authorizationToken, "Credential token was retrieved as nil")
-        XCTAssertNotNil(pulledCredential.refreshToken, "Credential refresh token was retrieved as nil")
-        XCTAssertNotNil(pulledCredential.expirationDate, "Credential expiry date was retrieved as nil")
-        
-        TSCRequestCredential.deleteCredentialWithIdentifier("credential")
-        
-        let deletedCredential = TSCRequestCredential.retrieveCredentialWithIdentifier("credential")
-        
-        XCTAssertNil(deletedCredential, "Credential was not deleted correctly from the keychain")
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        
+        XCTAssertNotNil(credential?.authorizationToken, "Authorization Token is nil")
+        XCTAssertNotNil(credential?.refreshToken, "Refresh Token is nil")
+        XCTAssertNotNil(credential?.expirationDate, "Expiry Date is nil")
     }
 }
