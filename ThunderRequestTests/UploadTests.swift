dd44d7174f394003d4c57494ff1a70aa3079605e
Simon Mitchell <simon@3sidedcube.com>
2018-12-17 14:30:22 +0000

    Removes upload tests as they don't work on Simulator

--- ThunderRequestTests/UploadTests.swift
+++ ThunderRequestTests/UploadTests.swift
@@ -12,24 +12,5 @@ import XCTest
 
 class UploadTests: XCTestCase {
     
-    let requestBaseURL = URL(string: "https://httpbin.org/")!
-    
-    func testUploadFromURLSucceeds() {
-        
-        guard let fileURL = Bundle(for: UploadTests.self).url(forResource: "350x150", withExtension: "png") else {
-            fatalError("Didn't get filepath for test image")
-        }
-        let requestController = RequestController(baseURL: requestBaseURL)
-        
-        let finishExpectation = expectation(description: "App should correctly upload file to server")
 
-        requestController.uploadFile(fileURL, to: "post", progress: { (progress, _, _) in
-            
-            XCTAssertTrue(progress >= 0.0 && progress <= 1.0)
-            
-        }) { (response, url, error) in
-            
-            finishExpectation.fulfill()
-        }
-    }
 }
